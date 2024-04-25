c8fb9420846114b2b35b16dcd57036074757720a
ulja4a <ulja4a@gmail.com>
2024-04-25 23:51:07 +0300

    init: start todo list and start markup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="todo">
+      <div className="todo__sidebar">
+        <button>Hello</button>
+      </div>
+      <div className="todo__tasks">
+        bdgjglgilh;l
+      </div>
+    </div>
+  );
+}
+
+export default App;
